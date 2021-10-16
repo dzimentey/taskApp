7ab5c192f4e0818d78f5badd11566a0c32c7a30c
dzimentey <dzimentey@gmail.com>
2021-10-16 13:36:20 +0300

    add: types for reducer

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,4 +1,8 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+type ActionType =   {type: 'check', payload: number} | {type: 'sort', payload: 'up'|'down'}
+
+export const homeWorkReducer = (state: Array<UserType>, action: ActionType): Array<UserType> => { // need to fix any
     switch (action.type) {
         case 'sort': {
             // need to fix
