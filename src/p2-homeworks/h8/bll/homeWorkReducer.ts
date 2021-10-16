7b6c3f757977b3f6834d3e28e8ba05ec916898ca
dzimentey <dzimentey@gmail.com>
2021-10-16 13:44:25 +0300

    add: check for age

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -2,15 +2,15 @@ import {UserType} from "../HW8";
 
 type ActionType =   {type: 'check', payload: number} | {type: 'sort', payload: 'up'|'down'}
 
-export const homeWorkReducer = (state: Array<UserType>, action: ActionType): Array<UserType> => { // need to fix any
+export const homeWorkReducer = (state: Array<UserType>, action: ActionType): Array<UserType> => { // need to fix any?
     switch (action.type) {
         case 'sort': {
             // need to fix
             return state
         }
         case 'check': {
-            // need to fix
-            return state
+            // need to fix?
+            return state.filter(u => u.age >= action.payload)
         }
         default: return state
     }
